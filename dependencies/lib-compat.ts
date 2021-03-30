6d183393f18a26735e808d6ce9271f14d47eb79c
nuts-n-bits <nuts-n-bits@outlook.com>
2021-03-29 21:22:09 -0400

    maintain lib compat

--- dependencies/lib-compat.ts
+++ dependencies/lib-compat.ts
@@ -1,10 +1,10 @@
-export { parse_url } from "../../lib-nnbc/src/functions/parse-url.ts"
-export type { ParsedUrl } from "../../lib-nnbc/src/functions/parse-url.ts"
-export * from "../../lib-nnbc/src/protocols/assert-passthrough.ts"
-export { async_sleep, sanitize_html } from "../../lib-nnbc/src/functions/misc.ts"
+export { parse_url } from "../../lib-nnbc/typescript/functions/parse-url.ts"
+export type { ParsedUrl } from "../../lib-nnbc/typescript/functions/parse-url.ts"
+export * from "../../lib-nnbc/typescript/protocols/assert-passthrough.ts"
+export { async_sleep, sanitize_html } from "../../lib-nnbc/typescript/functions/misc.ts"
 export { serve, ServerRequest } from "https://deno.land/std@0.90.0/http/server.ts"
 export type { Response } from "https://deno.land/std@0.90.0/http/server.ts"
 export { readableStreamFromIterable, readerFromStreamReader } from "https://deno.land/std@0.90.0/io/streams.ts"
-export { QuickQueue } from "../../lib-nnbc/src/functions/data-structure/quick-queue.ts"
+export { QuickQueue } from "../../lib-nnbc/typescript/functions/data-structure/quick-queue.ts"
 
 
